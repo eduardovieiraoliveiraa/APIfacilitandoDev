fe1a9e187a6d60d125df2cfdb84ade777d3aecfc
Eduardo Vieira Oliveira <eduardovieiraoliveiraa@gmail.com>
2022-06-30 19:22:17 -0300

    adc swagger na API

--- src/main/java/br/com/facilitandodev/resource/FacilitandoAPIResource.java
+++ src/main/java/br/com/facilitandodev/resource/FacilitandoAPIResource.java
@@ -6,11 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.facilitandodev.model.Endereco;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/v1/")
 public interface FacilitandoAPIResource {
 	
+	@ApiResponses(value = {
+		    @ApiResponse(code = 200, message = "Retorna o endereço do CEP informado"),
+		    @ApiResponse(code = 204, message = "Endereço não encontrado"),
+		    @ApiResponse(code = 500, message = "Foi gerada uma exceção"),
+		})
 	@GetMapping("consultar-cep/{cep}")
 	Endereco consultarCep(@PathVariable("cep") String cep);
 
